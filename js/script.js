ca1010e28acdc2d7ebac02e483fbba3daa158127
urusoff140272 <107813230+urusoff140272@users.noreply.github.com>
2024-04-19 09:28:35 +0300

    feat: add hdml and css sections header, only, about and freends and slider section friends

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-"use strict"
+const swiper = new Swiper('.main-friends__slider', {
+
+    loop: true,
+    speed: 800,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    slidesPerView: 3,
+
+    spaceBetween: 90,
+
+
+
+    // breakpoints: {
+    //     320: {
+    //         slidesPerView: 1,
+    //     },
+    //     768: {
+    //         slidesPerView: 2,
+    //     },
+    //     990: {
+    //         slidesPerView: 3,
+    //     }
+    // },
+});
